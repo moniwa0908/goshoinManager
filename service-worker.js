13915adad852f9dd378539786cd718a668b7e430
moniwa0908 <ko6subi4syo@yahoo.co.jp>
2026-08-11 19:05:58 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='goshoin-v1-4';
+const CACHE='goshoin-v1-5';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
